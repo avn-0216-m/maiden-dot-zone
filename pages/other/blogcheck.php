ffdfa3725e2f3cedb20abd6166a8bf4ca731cbc2
avn-0216-m <maidenofthebirdeternal@gmail.com>
2022-09-26 00:29:41 +0100

    able to check if blog exists

--- pages/other/blogcheck.php
+++ pages/other/blogcheck.php
@@ -1,1 +1,9 @@
-<?php $ch = curl_init("https://bwaaaaaaawawawa.tumblr.com"); ?>
+<?php
+$req = curl_init("https://doggiewoggiez.tumblr.com");
+curl_setopt($req, CURLOPT_HEADER, true);
+curl_setopt($req, CURLOPT_NOBODY, true);
+curl_setopt($req, CURLOPT_RETURNTRANSFER, true);
+curl_exec($req);
+echo curl_getinfo($req, CURLINFO_HTTP_CODE);
+echo curl_error($req);
+?>
