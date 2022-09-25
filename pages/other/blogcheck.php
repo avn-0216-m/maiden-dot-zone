61fa221ab02a8b70a65d3b1ef93a16fff7a0e4ad
avn-0216-m <maidenofthebirdeternal@gmail.com>
2022-09-26 00:33:22 +0100

    php script reads from query string

--- pages/other/blogcheck.php
+++ pages/other/blogcheck.php
@@ -1,9 +1,13 @@
 <?php
-$req = curl_init("https://doggiewoggiez.tumblr.com");
+
+$url1 = "https://";
+$url2 = ".tumblr.com";
+
+$req = curl_init($url1 . $_GET["name"] . $url2);
 curl_setopt($req, CURLOPT_HEADER, true);
 curl_setopt($req, CURLOPT_NOBODY, true);
 curl_setopt($req, CURLOPT_RETURNTRANSFER, true);
 curl_exec($req);
-echo curl_getinfo($req, CURLINFO_HTTP_CODE);
-echo curl_error($req);
+
+echo((bool)curl_getinfo($req, CURLINFO_HTTP_CODE) == "200");
 ?>
