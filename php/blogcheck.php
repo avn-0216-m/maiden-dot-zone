e0a0e063daabe1957bd61aba01f8c168554d60a0
avn-0216-m <maidenofthebirdeternal@gmail.com>
2022-10-02 18:24:37 +0100

    php error display enabled

--- php/blogcheck.php
+++ php/blogcheck.php
@@ -1,5 +1,7 @@
 <?php
 
+ini_set('display_errors', 1);
+
 $url1 = "https://";
 $url2 = ".tumblr.com";
 
@@ -8,8 +10,6 @@ curl_setopt($req, CURLOPT_HEADER, true);
 curl_setopt($req, CURLOPT_NOBODY, true);
 curl_setopt($req, CURLOPT_RETURNTRANSFER, true);
 curl_exec($req);
-
-echo(curl_error($req));
 
 if(curl_getinfo($req, CURLINFO_HTTP_CODE) == "200"){
     echo 1;
