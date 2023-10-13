f0379928a5f63d68b37f725fb639a96c8f745a0f
avn-0216-m <maidenofthebirdeternal@gmail.com>
2023-10-13 22:45:47 +0100

    added table of contents

--- pages/voidstranger/generator.py
+++ pages/voidstranger/generator.py
@@ -5,28 +5,32 @@ import jinja2
 with open("levels.yaml", "r") as file:
 
     root = yaml.safe_load(file)
+
+    anchors = []
+    print(root)
     
-    # Print some data.
     for domain in root['Grey']:
+        # Get all normal numeric anchors
         for domain_name, domain_floors in domain.items():
-            print("------")
-            print(domain_name)
             if domain_floors:
                 for floor in domain_floors:
-                    for dialogue_name, dialogues in floor.get("Void Memory", {}).items():
-                        print(dialogue_name)
-                        print(dialogues)
-                        print("----")
+                    to_test = floor.get("Floor", "???")
+                    if re.match(r"^[0-9]{3}$", to_test):
+                        print(f"Valid: {to_test}")
+                        anchors.append(to_test)
+                    else:
+                        print(f"Invalid: {to_test}")
 
-
-        print(root['Grey'])
+        anchors.sort()
+        print("All anchors:")
+        print(anchors)
 
         # Templatify w/ Jinja
         templateLoader = jinja2.FileSystemLoader(searchpath="./")
         templateEnv = jinja2.Environment(loader=templateLoader)
         TEMPLATE_FILE = "guide_template.html"
         template = templateEnv.get_template(TEMPLATE_FILE)
-        output = template.render(root = root)
+        output = template.render(root = root, anchors = anchors)
         with open("guide.html", "w+") as file:
             file.write(output)
         
